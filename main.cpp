7fdab5345eaa0193e59bd77ea71a713195217179
_sky2009 <106133518+736mo@users.noreply.github.com>
2023-04-15 13:14:59 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define int long long
+#define rg register
+#define pb push_back
+#define pp pop_back
+#define INF 0x7f7f7f7f
+#define i64 __int64
+#define i128 __int128
+#define PII pair<int, int>
+#define PDD pair<double, double>
+#define x first
+#define y second
+#define VI vector<int>
+#define VD vector<double>
+#define VPI vector<PII>
+#define VPD vector<PDD>
+#define mp make_pair
+#define bg begin
+#define all(x) x.bg(),x.end()
+#define PQ priority_queue
+#define GT greater
+#define rep(i, l, r) for (int i = (l); i <= (r); i++)
+#define per(i, l, r) for (int i = (l); i >= (r); i--)
+#define lowbit(x) (x & -x)
+#define gcd(x, y) __gcd(x, y)
+#define lcm(x, y) (x / gcd(x, y) * y)
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+inline int read(){int x=0,f=1;char ch=getchar();while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}while(ch>='0'&&ch<='9'){x=(x<<3)+(x<<1)+(ch^48);ch=getchar();}x*=f;return x;}
+inline void write(int x) {if(x<0)putchar('-'),x=-x;if(x>9)write(x/10);putchar(x%10+'0');return;}
+#define open(s) freopen(s".in", "r", stdin);freopen(s".out", "w", stdout);
+#define pdY cout<<"YES\n";
+#define pdN cout<<"NO\n";
+#define pdy cout<<"Yes\n";
+#define pdn cout<<"No\n";
+
+signed main() {
+    fast;
+
+    return 0;
+}
